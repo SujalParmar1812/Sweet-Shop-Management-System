6af13f58cf09200714ddbe13b5b3c16d98e54b34
Sujal Parmar <sujalparmar1812@gmail.com>
2025-07-16 18:55:21 +0530

    RED:Added test to delete sweets

--- tests/sweetShop.test.js
+++ tests/sweetShop.test.js
@@ -24,5 +24,16 @@ describe('sweet shop management', () => {
     const sweets = shop.getAllSweets();
     expect(sweets.length).toBeGreaterThan(1);
   })
+
+  test('should delete a sweet by id', () => {
+    const originalLength = shop.getAllSweets().length;
+    const sweetToDelete = shop.getAllSweets()[shop.getAllSweets().length-1];
+    shop.deleteSweet(sweetToDelete.id);
+
+    const afterDelete = shop.getAllSweets().length;
+    expect(afterDelete.length).toBe(originalLength-1);
+    expect(afterDelete.find(s=>s.id===sweetToDelete.id)).toBeUndefined();
+  })
+  
   
 });
