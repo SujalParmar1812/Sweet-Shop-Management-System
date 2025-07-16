8082c9a31ba2a6e05371cbede7e614d1e0d5097e
Sujal Parmar <sujalparmar1812@gmail.com>
2025-07-16 18:04:43 +0530

    added test for adding sweets

---
+++ tests/sweetShop.test.js
@@ -0,0 +1,22 @@
+
+
+describe('sweet shop management', () => {
+  let shop;
+
+  beforeEach(() => {
+    shop = new SweetShop();
+  });
+
+  test('should add a sweet and check by array length', () => {
+    const sweet = {
+      id: 13,
+      name: 'Badam Halwa',
+      category: 'Nut-based',
+      price: 50,
+      quantity: 10
+    };
+
+    shop.addSweet(sweet);
+    expect(shop.sweets.length).toBe(13);
+  });
+});
